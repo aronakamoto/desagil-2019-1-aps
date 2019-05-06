949b01ef8e401f805950c183370be5dabf10b9f3
Aronakamoto <aronakamoto@outlook.com>
2019-05-06 18:18:24 -0300

    Work

--- src/main/java/br/pro/hashi/ensino/desagil/aps/model/HalfAdder.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/HalfAdder.java
@@ -1,4 +1,63 @@
 package br.pro.hashi.ensino.desagil.aps.model;
 
 public class HalfAdder extends Gate {
+
+    private final NandGate nandOne;
+    private final NandGate nandTwo;
+    private final NandGate nandThree;
+
+    private final NandGate nandSum;
+    private final NandGate nandCarry;
+
+    public HalfAdder() {
+        super("Half-Adder", 2, 2);
+
+        nandOne = new NandGate();
+        nandTwo = new NandGate();
+        nandThree = new NandGate();
+
+        nandSum = new NandGate();
+        nandCarry = new NandGate();
+
+        nandTwo.connect(1, nandOne);
+
+        nandThree.connect(0, nandOne);
+
+        nandSum.connect(0, nandTwo);
+        nandSum.connect(1, nandThree);
+
+        nandCarry.connect(0, nandOne);
+        nandCarry.connect(1, nandOne);
+
+    }
+
+    @Override
+    public boolean read(int outputPin) {
+
+        if (outputPin == 0) {
+            return nandSum.read();
+        } else {
+            return nandCarry.read();
+        }
+
+    }
+
+    @Override
+    public void connect(int inputPin, SignalEmitter emitter) {
+        switch (inputPin) {
+            case 0:
+                nandOne.connect(0, emitter);
+                nandOne.connect(1, emitter);
+                break;
+            case 1:
+                nandTwo.connect(0, emitter);
+                break;
+            case 2:
+                nandThree.connect(1, emitter);
+                break;
+
+            default:
+                throw new IndexOutOfBoundsException(inputPin);
+        }
+    }
 }
